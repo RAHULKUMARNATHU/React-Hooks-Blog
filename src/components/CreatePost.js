cba44fc2c8d4bc02b7d62b7a53d0503c8f228e57
Rahul <rahulvaidya148@gmail.com>
2022-07-05 18:58:16 +0530

    commit :: custom hooks

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react';
-
+// import React, { useState } from 'react';
+import {useFormInput} from '../hooks'
 import { firestore } from '../firebase';
 
 function CreatePost() {
-  const [title, setTitle] = useState();
-  const [subTitle, setSubTitle] = useState();
-  const [content, setContent] = useState();
+  const title = useFormInput('');
+  const subTitle = useFormInput('');
+  const content = useFormInput('');
 
   function handleSubmit(e) {
     e.preventDefault();
 
-   
     firestore.collection('posts').add({
-      title,
-      content,
-      subTitle,
-      createdAt : new Date(),
+      title    : title.value , 
+      content  : content.value,
+      subTitle : subTitle.value ,
+      createdAt: new Date(),
     });
-
 
     console.log('Title', title);
     console.log('subTitle', subTitle);
     console.log('Content', content);
-
   }
 
   return (
@@ -31,20 +28,18 @@ function CreatePost() {
       <form onSubmit={handleSubmit}>
         <div className="form-field">
           <label>Title</label>
-          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+          <input {...title} />
         </div>
         <div className="form-field">
           <label> Subtitle</label>
           <input
-            value={subTitle}
-            onChange={(e) => setSubTitle(e.target.value)}
+            {...subTitle}
           />
         </div>
         <div className="form-field">
           <label>Content</label>
           <textarea
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            {...content}
           ></textarea>
         </div>
         <button className="create-post-btn">Create Post</button>
