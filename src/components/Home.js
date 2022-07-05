03f4276104ac8350db39c609e0bcaf4b55ee064b
Rahul <rahulvaidya148@gmail.com>
2022-07-05 23:14:41 +0530

    commit to Home page implementation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { firestore } from '../firebase';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <div>Home</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    firestore
+      .collection('posts')
+      .get()
+      .then((snapshot) => {
+        const posts = snapshot.docs.map((doc) => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        });
+        console.log('posts', posts);
+        setPosts(posts);
+      });
+  }, []);
+  return (
+    <div className="home">
+      <h1>Tech Blog</h1>
+      <div id="blog-by">Rahul</div>
+
+      {posts.map((post, index) => (
+        <div className="post" key={`post-${index}`}>
+          <Link to={`/post/${post.id}`}>
+            <h3>{post.title}</h3>
+          </Link>
+          <p>{post.subTitle}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Home;
