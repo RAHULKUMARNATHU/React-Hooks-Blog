1a18f36b819fdfd760f62de332e360cc459db0b2
Rahul <rahulvaidya148@gmail.com>
2022-07-06 16:54:37 +0530

    commit :: resolved createPost

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,12 +1,14 @@
 // import React, { useState } from 'react';
 import { useFormInput } from '../hooks';
 import { firestore } from '../firebase';
+import { toast, Toaster } from 'react-hot-toast';
 
 function CreatePost() {
   const title = useFormInput('');
   const subTitle = useFormInput('');
   const content = useFormInput('');
 
+  
   function handleSubmit(e) {
     e.preventDefault();
 
@@ -14,33 +16,42 @@ function CreatePost() {
       title: title.value,
       content: content.value,
       subTitle: subTitle.value,
+
       createdAt: new Date(),
     });
-
+   
+    toast.success('Successfully Created!')
+    title.handleReset();
+    subTitle.handleReset();
+    content.handleReset();
+    
     console.log('Title', title);
     console.log('subTitle', subTitle);
     console.log('Content', content);
   }
 
   return (
-    <div className="create-post">
-      <h1>Create Post</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="form-field">
-          <label>Title</label>
-          <input {...title} />
-        </div>
-        <div className="form-field">
-          <label> Subtitle</label>
-          <input {...subTitle} />
-        </div>
-        <div className="form-field">
-          <label>Content</label>
-          <textarea {...content}></textarea>
-        </div>
-        <button className="create-post-btn">Create Post</button>
-      </form>
-    </div>
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="create-post">
+        <h1>Create Post</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="form-field">
+            <label>Title</label>
+            <input {...title} />
+          </div>
+          <div className="form-field">
+            <label> Subtitle</label>
+            <input {...subTitle} />
+          </div>
+          <div className="form-field">
+            <label>Content</label>
+            <textarea {...content}></textarea>
+          </div>
+          <button className="create-post-btn">Create Post</button>
+        </form>
+      </div>
+    </>
   );
 }
 
--- src/hooks.js
+++ src/hooks.js
@@ -2,15 +2,17 @@ import { useState } from 'react';
 
 export function useFormInput(initialValue) {
   const [value, setValue] = useState(initialValue);
-  
 
   function handleChange(e) {
     setValue(e.target.value);
-
   }
 
+  function handleReset() {
+    setValue('');
+  }
   return {
     value,
     onChange: handleChange,
+    handleReset,
   };
 }
